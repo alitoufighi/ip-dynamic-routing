59fe3d3a95f28f18a6be77575dad8d0f1e6b6042
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-06-04 16:33:17 +0430

    add up and down methods for interface

--- py/interface.py
+++ py/interface.py
@@ -6,6 +6,13 @@ class Interface:
         self.addr = Address(ip, port)
         self.my_virt_ip = my_virt_ip
         self.peer_virt_ip = peer_virt_ip
+        self.is_up = True
+
+    def up(self):
+        self.is_up = True
+
+    def down(self):
+        self.is_up = False
 
     def __repr__(self):
         return f"{self.addr.ip} {self.addr.port} {self.my_virt_ip} {self.peer_virt_ip}"
